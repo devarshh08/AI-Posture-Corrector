07cb7b9e86727ac923cb6dfe0c508f020547dac7
Devarsh Shah <devarsh.s9521@gmail.com>
2025-08-14 01:58:49 +0530

    added mediapipe and skeleton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import mediapipe as mp
 
 webcam = cv2.VideoCapture(0)
 
@@ -10,8 +11,30 @@ frame_height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'mpv4')
 out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
+#we will be detecting pose with mediapipe
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_drawing = mp.solutions.drawing_utils
+
+#this will load a pretrained neural network into memory
+
 while True:
     ret, frame = webcam.read()
+    
+    #mediapipe was trained on RGB, while opencv reads in BGR; so we convert each frame from BGR to RGB
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    #we will be processing the image to find the pose
+    results = pose.process(image_rgb)
+
+    #we will draw skeleton on frame before displaying it
+    #drawing pose annotation on the original frame
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(
+            frame, 
+            results.pose_landmarks, 
+            mp_pose.POSE_CONNECTIONS
+        )
 
     #writing frame to output file
     out.write(frame)
@@ -26,3 +49,4 @@ while True:
 webcam.release()
 out.release()
 cv2.destroyAllWindows()
+
